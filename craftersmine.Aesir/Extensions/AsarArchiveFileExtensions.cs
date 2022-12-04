f104ee084fbd981b731cea1285f87c17b9d3be75
craftersmine <rodion.morozov.01@gmail.com>
2022-12-04 20:35:33 +0700

    Added AsarArchiveFile extensions

---
+++ craftersmine.Aesir/Extensions/AsarArchiveFileExtensions.cs
@@ -0,0 +1,18 @@
+﻿using craftersmine.Asar.Net;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace craftersmine.Aesir.Extensions
+{
+    public static class AsarArchiveFileExtensions
+    {
+        public static bool CheckIfDirectory(this AsarArchiveFile file)
+        {
+            return file.Files is not null && file.Files.Any();
+        }
+    }
+}
