cbbc37643c2c372ff53ea2f60516ee9e0c3b9e8b
craftersmine <rodion.morozov.01@gmail.com>
2022-12-04 20:35:55 +0700

    Added static data class

---
+++ craftersmine.Aesir/StaticData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace craftersmine.Aesir
+{
+    public static class StaticData
+    {
+        public static Archive OpenedArchive { get; set; }
+    }
+}
