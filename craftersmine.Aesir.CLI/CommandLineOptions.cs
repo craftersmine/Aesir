2aae7ed8f53c54e00cf5f69a82e247c1d5ec1594
craftersmine <rodion.morozov.01@gmail.com>
2022-12-15 00:12:21 +0700

    Added CLI project

---
+++ craftersmine.Aesir.CLI/CommandLineOptions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommandLine;
+
+namespace craftersmine.Aesir.CLI
+{
+    public class Options
+    {
+
+        [Option] public IEnumerable<string> Paths { get; set; }
+
+        [Option('v', "verbose")] public bool Verbose { get; set; }
+
+        [Option('x', "extract")]
+        public bool Extract { get; set; }
+
+        [Option('p', "pack")]
+        public bool Pack { get; set; }
+
+        [Option('s', "sort")]
+        public bool SortFiles { get; set; }
+
+        [Option('o', "output", Required = true)] public string OutputFile { get; set; }
+    }
+}
---
+++ craftersmine.Aesir.CLI/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Diagnostics;
+using CommandLine;
+using craftersmine.Asar.Net;
+
+namespace craftersmine.Aesir.CLI
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            var parser = new Parser(with =>
+            {
+                with.EnableDashDash = true;
+                with.AutoVersion = true;
+            });
+            var result = Parser.Default.ParseArguments<Options>(args).WithParsed((options) =>
+            {
+                if (!options.Extract && options.Pack)
+                {
+                    Pack(options);
+                }
+            }).WithNotParsed((err) =>
+            {
+                Console.WriteLine(err);
+            });
+
+            while (isWorking)
+            {
+
+            }
+        }
+
+        public async static void Pack(Options options)
+        {
+            if (options.Verbose)
+                Console.WriteLine("Creating archive builder...");
+            string? outDir = Path.GetDirectoryName(options.OutputFile);
+            if (string.IsNullOrWhiteSpace(outDir))
+                outDir = Environment.CurrentDirectory;
+
+            AsarArchivePackerDataBuilder builder = AsarArchivePackerDataBuilder.CreateBuilder(outDir, Path.GetFileNameWithoutExtension(options.OutputFile));
+
+            if (options.Verbose)
+                Console.WriteLine("Adding files to path...");
+
+            foreach (var path in options.Paths)
+            {
+                FileAttributes attrs = File.GetAttributes(path);
+
+                if (attrs.HasFlag(FileAttributes.Directory))
+                {
+                    if (options.Verbose)
+                        Console.WriteLine("Adding directory to archive: " + path);
+                    builder.AddDirectory(path);
+                }
+                else
+                {
+                    if (options.Verbose)
+                        Console.WriteLine("Adding file to archive: " + path);
+                    builder.AddFile(path);
+                }
+            }
+
+            if (options.SortFiles)
+            {
+                builder.PerformFileSort(true);
+                if (options.Verbose)
+                    Console.WriteLine("Files will be sorted before packing");
+            }
+
+            AsarArchivePackerData data = builder.CreateArchiveData();
+
+            Console.WriteLine("Packing archive at " + Path.Combine(data.OutputDirectoryPath, data.ArchiveName + ".asar"));
+
+            AsarArchivePacker packer = new AsarArchivePacker(data);
+            CancellationTokenSource tokenSource = new CancellationTokenSource();
+
+            packer.StatusChanged += Packer_StatusChanged;
+
+            await packer.PackAsync(tokenSource.Token);
+        }
+
+        private static bool isWorking = true;
+
+        private static void Packer_StatusChanged(object? sender, AsarPackingEventArgs e)
+        {
+            isWorking = e.PackingStatus != AsarPackingStatus.Completed;
+            (int x, int y) = Console.GetCursorPosition();
+            Console.SetCursorPosition(0, y);
+            Console.Write(new string(' ', Console.WindowWidth));
+            Console.SetCursorPosition(0, y);
+            string? currentFilePath = e.CurrentFileData is not null ? e.CurrentFileData.GetPathInArchive() : " ... ";
+            Console.Write("{0} | {1}/{2} | {3:F2} % | {4}", e.PackingStatus, e.CurrentFile, e.TotalFiles, ((float)e.CurrentFile / (float)e.TotalFiles) * 100, currentFilePath);
+        }
+    }
+}
